149504cf35cecb49fa080b5d3e6d11cc82052223
Chimezie Ugbuaja <ugbuajachimezie@gmail.com>
2023-10-16 22:08:10 -0400

    Needed to merge state hw

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,80 @@
 import React from "react";
 import "./App.css";
+import e_logo from "./images/OG_Philadelphia_Eagles.jpg";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
+                <h1
+                    style={{
+                        height: "50px",
+                        fontSize: "25px",
+                        color: "gold",
+                        backgroundColor: "darkgreen"
+                    }}
+                >
+                    By: Chimezie Ugbuaja
+                </h1>
             </header>
+            ;
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. By Chimezie Ugbuaja
+                Hello World! Edit <code>src/App.tsx</code> and save. This page
+                will automatically reload.
             </p>
+            <div>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </div>{" "}
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            First column.
+                            <div
+                                style={{
+                                    border: "5px solid red",
+                                    padding: "1px",
+                                    backgroundColor: "white"
+                                }}
+                            >
+                                <div>
+                                    <ul>
+                                        <li>
+                                            This is{" "}
+                                            <span style={{ color: "red" }}>
+                                                colored text
+                                            </span>
+                                            .
+                                        </li>
+                                        <li>
+                                            The image on the right is the Eagles
+                                            logo.
+                                        </li>
+                                        <li>
+                                            This is looking pretty good so far.
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col>
+                            Second column.
+                            <div>
+                                <img
+                                    src={e_logo}
+                                    alt="Eagles Logo"
+                                    width="250"
+                                    height="125"
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
